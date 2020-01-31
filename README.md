c0f5f6a563b53d035e550019ef5fef4b5240f2f3
Francis-moon <12130091@qq.com>
2020-01-31 19:55:13 +0800

    Add the setting buttons, but the function need to updated in the next step.

--- README.md
+++ README.md
@@ -4,10 +4,11 @@
 <p>The AI is from : https://www.imooc.com/learn/639</p>
 
 <h2>The updates:</h2>
-<p>1, Set the background color of chess board to Yellow. --done<br>
-2, Add the sign for the last step.  --to do<br>
-3, Forbid the 3-3 connection when going first.  --to do<br>
-4, Set a table to choose <br>
-&nbsp&nbsp A: Whether the oppsite is human or computer?  --to do<br>
-&nbsp&nbsp B: Go first or last, use black or white?  --to do<br>
-&nbsp&nbsp C: Add the "1" sign for the last step.  -- to do</p>
+<p>1, Set the background color of chess board to Yellow. --done at 10pm 2020/01/30<br>
+2, Add the button of "singleVScomputer" or "double pVSp", and if choose ""singleVScomputer", display the new buttons about go "first" or "last". but the function is still needed to do.  --done at 8pm 2020/01/31<br>
+3, Change the overall function to make the "pVSp" is workable. -- to do<br>
+4, Adjust the computerAI to go "first". -- to do<br>
+5, Add the sign for the last step.  --to do<br>
+6, Forbid the 3-3 connection when going first.  --to do<br>
+
+
--- script.js
+++ script.js
@@ -1,7 +1,26 @@
 //improved by Francis
+var oset = document.getElementById("setting");
+var scolor = oset.getElementsByTagName("div");
+function single() {
+    scolor[0].className = "";
+}
+function double(){
+    alert("准备双人对弈 - 开始")
+    chess.onclick = doublePersons;
+}
+
+function first() {
+    alert("准备执黑vs电脑 - 开始")
+    chess.onclick = singleFirst;
+}
+function last() {
+    alert("准备执白vs电脑 - 开始")
+    chess.onclick = singleLast;
+}
+
+
 var chess = document.getElementById("chess");
 var context = chess.getContext("2d");
-
 context.strokeStyle="#BFBFBF";
 
 var logo = new Image();
@@ -21,16 +40,16 @@ var drawChessBoard =function(){
   }
 }
 
-var oneStep=function(i,j,me){
+var oneStep=function(i,j,black){  //落子函数-实际是在i,j处画出一枚棋子
     context.beginPath();
     context.arc(15 + i*30, 15 + j*30, 13, 0, 2*Math.PI);
     context.closePath();
     var gradient = context.createRadialGradient(15 + i*30 + 2, 15 + j*30 - 2, 13, 15 + i*30 + 2, 15 + j*30 - 2, 0)
-    if(me){
+    if(black){  //画一枚黑棋
         gradient.addColorStop(0, "#0A0A0A");
         gradient.addColorStop(1, "#636766");
     }
-    else{
+    else{    //白棋
         gradient.addColorStop(0, "#D1D1D1");
         gradient.addColorStop(1, "#F9F9F9");
     }
@@ -38,9 +57,9 @@ var oneStep=function(i,j,me){
     context.fill();
 }
 
-var me = true;
+var black = true;
 var chessBoard = [];
-for(var i=0; i<15; i++){
+for(var i=0; i<15; i++){  //初始化二维数组chessBoard
     chessBoard[i]=[];
     for(var j=0; j<15; j++) {
         chessBoard[i][j]=0
@@ -48,27 +67,21 @@ for(var i=0; i<15; i++){
 }
 
 var over=false; 
-chess.onclick = function(e) {
+var singleFirst = function firstVScomputer(e)  {   //执黑面对电脑，落子在e点
     if(over){
         return;
     }
     //电脑走棋
-    if(!me){
+    if(!black){
         return;
     }
     var x = e.offsetX;
     var y = e.offsetY;
     var i = Math.floor(x / 30);
     var j = Math.floor(y / 30);
-    if(chessBoard[i][j]==0){
-      oneStep(i,j,me);
-      //if(me){
+    if(chessBoard[i][j]==0){   //等于0表示该点可以落子
+      oneStep(i,j,black);
           chessBoard[i][j]=1;  //我落子
-      //}
-      //else{
-      //    chessBoard[i][j]=2;
-      //}
-      //me =!me;
       //定义赢法
       for(var k=0; k<count; k++) {
           if(wins[i][j][k]) {
@@ -81,36 +94,90 @@ chess.onclick = function(e) {
           }
       }
       if(!over){
-          me =!me;
+          black =!black;
           computerAI();
+      }
+    }
+}
+var singleLast = function lastVScomputer(e)  {   //执白面对电脑，落子在e点
+    if(over) {
+        return;
+    }
+    if(black) {
+        computerAI();
+        black =!black;
+    }
+    var x = e.offsetX;
+    var y = e.offsetY;
+    var i = Math.floor(x / 30);
+    var j = Math.floor(y / 30);
+    if(chessBoard[i][j]==0){   //等于0表示该点可以落子
+      oneStep(i,j,black);
+          chessBoard[i][j]=1;  //我落子
+      //定义赢法
+      for(var k=0; k<count; k++) {
+          if(wins[i][j][k]) {
+              myWin[k]++;
+              uWin[k] = 6; //异常情况
+              if(myWin[k] == 5) {
+                window.alert("恭喜，你赢了");
+                over = true;
+              }
+          }
+      }
+    }
+}
+
+var doublePersons = function pVSp(e)  {   //双人对弈，落子在e点
+    if(over) {
+        return;
+    }
+    var x = e.offsetX;
+    var y = e.offsetY;
+    var i = Math.floor(x / 30);
+    var j = Math.floor(y / 30);
+    if(chessBoard[i][j]==0){   //等于0表示该点可以落子
+      oneStep(i,j,black);
+      if(black) {
+          chessBoard[i][j]=1;  //记录黑棋落子
+        }
+        else {
+          chessBoard[i][j]=2;   //记录白棋落子
+        }
+       black =!black;
+      //定义赢法
+      for(var k=0; k<count; k++) {
+          if(wins[i][j][k]) {
+              myWin[k]++;
+              uWin[k] = 6; //异常情况
+              if(myWin[k] == 5) {
+                 if(black) {
+                 window.alert("恭喜，白棋赢了");
+                 over = true;
+                 }
+                 else {
+                 window.alert("恭喜，黑棋赢了");
+                 over = true;
+                 }
+              }
+          }
       }
     }
 }
 
 //赢法的数组
 var wins = [];
-for(var i=0;i<15;i++) {
+for(var i=0; i<15; i++) {
     wins[i]=[];
-    for(j=0;j<15;j++){
+    for(j=0; j<15; j++){
         wins[i][j]=[];
     }
 }
 
 var count=0;
 //所有横着5连的赢法；
-for(var i=0;i<15;i++){
-    for(j=0;j<11;j++){
-        //wins[0][0][0] = true;
-        //wins[0][1][0] = true;
-        //wins[0][2][0] = true;
-        //wins[0][3][0] = true;
-        //wins[0][4][0] = true;
-
-        //wins[0][1][1] = true;
-        //wins[0][2][1] = true;
-        //wins[0][3][1] = true;
-        //wins[0][4][1] = true;
-        //wins[0][5][1] = true;
+for(var i=0; i<15; i++){
+    for(j=0; j<11; j++){
         for(var k=0; k<5; k++){
             wins[i][j+k][count] = true;
         }
@@ -118,19 +185,8 @@ for(var i=0;i<15;i++){
     }
 }
 //所有竖着5连的赢法；
-for(var i=0;i<15;i++){
-    for(j=0;j<11;j++){
-        //wins[0][0][0] = true;
-        //wins[1][0][0] = true;
-        //wins[2][0][0] = true;
-        //wins[3][0][0] = true;
-        //wins[4][0][0] = true;
-
-        //wins[1][0][1] = true;
-        //wins[2][0][1] = true;
-        //wins[3][0][1] = true;
-        //wins[4][0][1] = true;
-        //wins[5][0][1] = true;
+for(var i=0; i<15; i++){
+    for(j=0; j<11; j++){
         for(var k=0; k<5; k++){
             wins[j+k][i][count] = true;
         }
@@ -138,19 +194,8 @@ for(var i=0;i<15;i++){
     }
 }
 //所有45度斜着5连的赢法；
-for(var i=0;i<11;i++){
-    for(j=0;j<11;j++){
-        //wins[0][0][0] = true;
-        //wins[1][1][0] = true;
-        //wins[2][2][0] = true;
-        //wins[3][3][0] = true;
-        //wins[4][4][0] = true;
-
-        //wins[0][1][1] = true;
-        //wins[1][2][1] = true;
-        //wins[2][3][1] = true;
-        //wins[3][4][1] = true;
-        //wins[4][5][1] = true;
+for(var i=0; i<11; i++){
+    for(j=0; j<11; j++){
         for(var k=0; k<5; k++){
             wins[i+k][j+k][count] = true;
         }
@@ -158,19 +203,8 @@ for(var i=0;i<11;i++){
     }
 } 
 //所有135度斜着5连的赢法；
-for(var i=0;i<11;i++){
-    for(j=14;j>3;j--){
-        //wins[0][14][0] = true;
-        //wins[1[13][0] = true;
-        //wins[2][12][0] = true;
-        //wins[3][11][0] = true;
-        //wins[4][10][0] = true;
-
-        //wins[0][13][1] = true;
-        //wins[1][12][1] = true;
-        //wins[2][11][1] = true;
-        //wins[3][10][1] = true;
-        //wins[4][9][1] = true;
+for(var i=0; i<11; i++){
+    for(j=14; j>3; j--){
         for(var k=0; k<5; k++){
             wins[i+k][j-k][count] = true;
         }
@@ -178,9 +212,9 @@ for(var i=0;i<11;i++){
     }
 } 
 
-console.log(count); //查看总共的赢法数量
+console.log("总共的赢法数量是",count); //查看总共的赢法数量
 
-//赢法的统计数组
+//赢法的统计数组，到5的时候就赢了
 var myWin=[];
 var uWin=[];
 
@@ -193,7 +227,7 @@ var computerAI = function() {
     var myScore = [];
     var uScore = [];
     var max = 0;
-    var u = 0; v = 0; //保存最高分数的点
+    var u = 0; v = 0; //u,v是最高分数max的点
     for(var i=0; i<15; i++){
         myScore[i] = [];
         uScore[i] = [];
@@ -231,7 +265,6 @@ var computerAI = function() {
                         else if(uWin[k] == 4){
                              uScore[i][j] += 20000;
                         }
-                        
                     }
                 }
                 //u,v是综合myScore和uScore的值
@@ -260,9 +293,10 @@ var computerAI = function() {
             }
         }
     }
+    console.log('max is', max); //查看权重
     //计算机落子
-    oneStep(u, v, false)
-    chessBoard[u][v] = 2;  //计算机落子
+    oneStep(u, v, false);  //false代表白棋
+    chessBoard[u][v] = 2;  //记录为白棋
     for(var k=0; k<count; k++) {
         if(wins[u][v][k]) {
             uWin[k]++;
@@ -274,6 +308,6 @@ var computerAI = function() {
         }
     }
     if(!over) {
-        me = !me;
+        black = !black;
     }
 }
